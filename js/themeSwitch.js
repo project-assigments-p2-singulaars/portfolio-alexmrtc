590777654a5e13f1b3fff11150bab192934ac304
LPCBCN044 <alexmrtc@gmail.com>
2024-03-14 18:31:40 +0100

    Initialize responsive theme switcher

---
+++ js/themeSwitch.js
@@ -0,0 +1,20 @@
+
+const lightText = "theme-light";
+const darkText = "theme-dark";
+
+const sunIcon = "sun.svg";
+const moonIcon = "moon.svg";
+
+localStorage.setItem("theme", lightText);
+
+function setTheme(icon, theme){
+    themeIconResponsive.src = icon;
+    localStorage.setItem("theme", theme);
+}
+
+let themeIconResponsive = document.getElementById("responsiveIcon");
+
+themeIconResponsive.addEventListener("click", () => {
+    console.log(localStorage.getItem("theme") === lightText)
+    localStorage.getItem("theme") === lightText ? setTheme(moonIcon, darkText) : setTheme(sunIcon, lightText)
+} )
